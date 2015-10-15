c9e9bb433d336a31ef3a9c00eaa89dc9b297fda9
Attila Domokos <adomokos@gmail.com>
2015-10-15 12:45:29 -0500

    Using ring response to make the code shorter

--- src/kashmir/handler.clj
+++ src/kashmir/handler.clj
@@ -2,14 +2,13 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [ring.util.response :as rr]
             [kashmir.logic :as logic]))
 
 (defroutes api-routes
     (GET "/" [] "Hello World")
     (GET "/members/:id{[0-9]+}" [id]
-         {:status 200
-          :headers {"Content-Type" "application/json; charset=utf-8"}
-          :body (logic/find-member (read-string id))})
+         (rr/response (logic/find-member (read-string id))))
     (route/not-found "Not Found"))
 
 (def app
