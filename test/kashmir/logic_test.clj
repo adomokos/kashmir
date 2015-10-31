debc055da395f3d18edad13fd9d69b0cdeb0cfe8
Attila Domokos <adomokos@gmail.com>
2015-10-31 13:25:54 -0500

    Stub out the data access function in the test

--- test/kashmir/logic_test.clj
+++ test/kashmir/logic_test.clj
@@ -1,9 +1,11 @@
 (ns kashmir.logic-test
   (:require [clojure.test :refer :all]
             [kashmir.logic :refer :all]
+            [kashmir.data :as data]
             [cheshire.core :as json]))
 
 (deftest find-member-by-id-test
   (testing "returns a JSON serialized member record"
+    (with-redefs [data/find-member (fn [id] [{:first_name "Paul"}])]
       (let [member (first (json/parse-string (find-member 2) true))]
-        (is (= "Paul" (:first_name member))))))
+        (is (= "Paul" (:first_name member)))))))
