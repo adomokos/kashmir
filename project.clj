81d40aa906908e815f16f53e00701124a6701405
Attila Domokos <adomokos@gmail.com>
2015-11-01 19:36:45 -0600

    Add JDBC logging to monitor activities

--- project.clj
+++ project.clj
@@ -8,10 +8,12 @@
                  [yesql "0.5.1"]
                  [compojure "1.4.0"]
                  [ring/ring-defaults "0.1.5"]
-                 [cheshire "5.5.0"]]
+                 [cheshire "5.5.0"]
+                 [com.googlecode.log4jdbc/log4jdbc "1.2"]]
   :clj-sql-up {:database "jdbc:postgresql://kashmir_user:password@localhost:5432/kashmir"
                :deps [[org.postgresql/postgresql "9.4-1201-jdbc41"]]}
   :ring {:handler kashmir.handler/app}
+  :resource-paths ["lib/slf4j-simple-1.7.12.jar"]
   :plugins  [[clj-sql-up "0.3.7"]
              [lein-ring "0.9.7"]]
   :main ^:skip-aot kashmir.core
--- src/kashmir/data.clj
+++ src/kashmir/data.clj
@@ -2,8 +2,9 @@
   (:require [yesql.core :refer [defqueries]]
             [clojure.java.jdbc :as jdbc]))
 
-(def db-spec {:classname "org.postgresql.Driver"
-              :subprotocol "postgresql"
+(def db-spec {:classname "net.sf.log4jdbc.DriverSpy"
+              :subprotocol "log4jdbc:postgresql"
+              ;:subprotocol "postgresql"
               :subname "//localhost:5432/kashmir"
               :user "kashmir_user"
               :password "password1"})
